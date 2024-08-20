cab7d7a8593845807b46362ed570adeb660ea098
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-08-20 15:21:38 +0700

    fix handle delete workout

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,13 +1,18 @@
 import { useDispatch } from "react-redux";
 import { deleteWorkout } from "../store/slices/workoutSlice";
+
 import axios from "axios";
 const WorkoutDetails = ({ workout }) => {
+
+
   const dispatch = useDispatch();
-  const handleDelete = async() => {
+  const handleDelete = async () => {
     const id = workout._id;
     try {
-      const response = await axios.delete(`http://localhost:4000/api/workouts/${id}`);
-      
+      const response = await axios.delete(
+        `http://localhost:4000/api/workouts/${id}`
+      );
+
       if (response.status === 200) {
         dispatch(deleteWorkout(id));
       }
@@ -16,9 +21,7 @@ const WorkoutDetails = ({ workout }) => {
     }
   };
 
-  const handleEdit = () => {
-    console.log("Edit");
-  };
+ 
 
   return (
     <div className=" workout-details">
@@ -36,9 +39,8 @@ const WorkoutDetails = ({ workout }) => {
         <div className="button" onClick={handleDelete}>
           Delete
         </div>
-        <div className="button" onClick={handleEdit}>
-          Edit
-        </div>
+     
+     
       </span>
     </div>
   );
