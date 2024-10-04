ff605c3d1252317bebd869b8aa020534842c1325
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-10-05 05:18:41 +0700

    bycrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const VfToken = require("../models/vfTokenModel");
 const sendEmail = require("../utils/sendEmail");
